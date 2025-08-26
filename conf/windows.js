aac936d03bf6ca5b1ac71a4c2875f68f25672f54
ℋℯ𝒳𝒾𝓃ℊ <@>
2025-08-26 16:55:05 +0800

    feat(conf): 添加 AI 代理配置和规则集
    - 新增 windows.js 配置文件，包含 DNS 配置、代理组、规则集等
    - 创建 windows.txt规则集文件，用于 AI代理
    - 配置文件中

---
+++ conf/windows.js
@@ -0,0 +1,133 @@
+// 国内DNS服务器
+const domesticNameservers = [
+    "https://223.5.5.5/dns-query", // 阿里DoH
+    "https://doh.pub/dns-query" // 腾讯DoH
+];
+// 国外DNS服务器
+const foreignNameservers = [
+    "https://208.67.222.222/dns-query", // OpenDNS
+    "https://77.88.8.8/dns-query", //YandexDNS
+    "https://1.1.1.1/dns-query", // CloudflareDNS
+    "https://8.8.4.4/dns-query", // GoogleDNS
+
+];
+// DNS配置
+const dnsConfig = {
+    "enable": true,
+    "listen": "0.0.0.0:1053",
+    "ipv6": false,
+    "prefer-h3": false,
+    "respect-rules": true,
+    "use-system-hosts": false,
+    "cache-algorithm": "arc",
+    "enhanced-mode": "fake-ip",
+    "fake-ip-range": "198.18.0.1/16",
+    "fake-ip-filter": [
+        // 本地主机/设备
+        "+.lan",
+        "+.local",
+        // // Windows网络出现小地球图标
+        "+.msftconnecttest.com",
+        "+.msftncsi.com",
+        // QQ快速登录检测失败
+        "localhost.ptlogin2.qq.com",
+        "localhost.sec.qq.com",
+        // 追加以下条目
+        "+.in-addr.arpa",
+        "+.ip6.arpa",
+        "time.*.com",
+        "time.*.gov",
+        "pool.ntp.org",
+        // 微信快速登录检测失败
+        "localhost.work.weixin.qq.com"
+    ],
+    "default-nameserver": ["223.5.5.5", "1.2.4.8"],//可修改成自己ISP的DNS
+    "nameserver": [...foreignNameservers],
+    "proxy-server-nameserver": [...domesticNameservers],
+    "nameserver-policy": {
+        "geosite:private,cn": domesticNameservers
+    }
+};
+// 规则集通用配置
+const ruleProviderCommon = {
+    "type": "http",
+    "format": "yaml",
+    "interval": 86400
+};
+// 规则集配置
+const ruleProviders = {
+    "AI": {
+        ...ruleProviderCommon,
+        "behavior": "classical",
+        "url": "https://raw.githubusercontent.com/IHexing/conf/refs/heads/main/list/windows.txt",
+        "path": "./ruleset/ai/AI.yaml"
+    }
+};
+// 规则
+const rules = [
+    // 代理
+    "RULE-SET,AI,AI",
+    // 直连
+    "MATCH,直连"
+];
+// 代理组通用配置
+const groupBaseOption = {
+    "interval": 300,
+    "timeout": 3000,
+    "url": "https://www.google.com/generate_204",
+    "lazy": true,
+    "max-failed-times": 3,
+    "hidden": false
+};
+
+// 程序入口
+function main(config) {
+    const proxyCount = config?.proxies?.length ?? 0;
+    const proxyProviderCount =
+        typeof config?.["proxy-providers"] === "object" ? Object.keys(config["proxy-providers"]).length : 0;
+    if (proxyCount === 0 && proxyProviderCount === 0) {
+        throw new Error("配置文件中未找到任何代理");
+    }
+
+    // 覆盖原配置中DNS配置
+    config["dns"] = dnsConfig;
+
+    // 覆盖原配置中的代理组
+    config["proxy-groups"] = [
+        {
+            ...groupBaseOption,
+            "name": "AI",
+            "type": "url-test",
+            "interval": 120,
+            "tolerance": 200,
+            "include-all": true,
+            "filter": "^(?!.*(美国)).*$",
+            "icon": "https://fastly.jsdelivr.net/gh/clash-verge-rev/clash-verge-rev.github.io@main/docs/assets/icons/speed.svg"
+        },
+
+        {
+            ...groupBaseOption,
+            "name": "直连",
+            "type": "select",
+            "proxies": ["DIRECT"],
+            "include-all": true,
+            "icon": "https://fastly.jsdelivr.net/gh/clash-verge-rev/clash-verge-rev.github.io@main/docs/assets/icons/link.svg"
+        }
+    ];
+
+    // 覆盖原配置中的规则
+    config["rule-providers"] = ruleProviders;
+    config["rules"] = rules;
+    // 添加判断
+    if (config["proxies"]) {
+        config["proxies"].forEach(proxy => {
+            // 为每个节点设置 udp = true
+            proxy.udp = true
+
+        })
+    }
+    // 返回修改后的配置
+    return config;
+
+}
+
